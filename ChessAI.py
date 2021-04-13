2ef28475c227069350246bbb99c2813abf85cc3e
aidanbossart <aidanbossart@gmail.com>
2021-04-13 14:41:21 -0400

    Pruning

--- ChessAI.py
+++ ChessAI.py
@@ -1,5 +1,6 @@
 import chess
 import random
+import math
 
 class ChessAI():
     def __init__(self, board):
@@ -20,21 +21,69 @@ class ChessAI():
         return move
     
     def getPieceValue(self, piece):
-        print(self.values.get(piece.piece_type))
-        return self.values.get(piece.piece_type)
+        #if(piece.color == chess.WHITE):
+        #    self.values.get(piece.piece_type)
+        #elif(piece.color == chess.BLACK):
+        #    self.values.get(piece.piece_type)
+        return self.values.get(piece.piece_type) if piece.color == chess.WHITE else -1*self.values.get(piece.piece_type)
     
     def getBestMove(self):
+        print("getting best move")
+        move = self.minimaxRoot(3, True)
+        #move = self.minimax(3, -9999, 9999, True)
+        print(move)
+
+        return move
+    
+    def minimaxRoot(self, depth, isMaximizing):
         best = None
         bestValue = -9000
 
         for move in self.board.legal_moves:
-            if(self.board.is_capture(move) == True):
-                piece = self.board.piece_at(move.to_square)
-                if(self.getPieceValue(piece) > bestValue):
-                    best = move
-                    bestValue = self.getPieceValue(piece)
-        
-        if(best == None):
-            best = list(self.board.legal_moves)[random.randint(0, self.board.legal_moves.count()-1)]
+            self.board.push(move)
+            val = max(bestValue, self.minimax(depth - 1, -9999, 9999, False))
+            print(val)
+            self.board.pop()
+            if(val > bestValue):
+                bestValue = val
+                best = move
         
         return best
+
+    def evaluation(self):
+        eval = 0
+        for i in range(0, 64):
+            #print(self.board.piece_at(chess.square(i - (8*math.floor(i/8)), 7 - math.floor(i/8))))
+            if(self.board.piece_at(chess.square(i - (8*math.floor(i/8)), 7 - math.floor(i/8))) != None):
+                eval = eval + self.getPieceValue(self.board.piece_at(chess.square(i - (8*math.floor(i/8)), 7 - math.floor(i/8))))
+        return eval
+
+
+    def minimax(self, depth, alpha, beta, isMaximizing):
+        if(depth == 0):
+            #print("EVAL = "+ str(-self.evaluation()))
+            return -self.evaluation()
+        
+        moves = self.board.legal_moves
+
+        if(isMaximizing):
+            best = -9999
+            for move in moves:
+                self.board.push(move)
+                best = max(best, self.minimax(depth -1, alpha, beta, not isMaximizing))
+                print("BEST = "+str(best))
+                self.board.pop()
+                alpha = max(alpha, best)
+                if(beta <= alpha):
+                    break
+            return best
+        else:
+            best = 9999
+            for move in moves:
+                self.board.push(move)
+                best = min(best, self.minimax(depth -1, alpha, beta, not isMaximizing))
+                self.board.pop()
+                beta = min(beta, best)
+                if(beta <= alpha):
+                    break
+            return best
--- ChessEngine.py
+++ ChessEngine.py
@@ -2,19 +2,7 @@ import chess
 
 class GameState():
     def __init__(self):
-
         self.board = chess.Board()
-
-        self.whiteToMove = True
-        self.moveLog = []
-        print(self.board)
     
     def makeMove(self, code):
         self.board.push(code)
-    
-    def undoMove(self):
-        if(len(self.moveLog) != 0):
-            move = self.moveLog.pop()
-            self.board[move.startRow][move.startCol] = move.pieceMoved
-            self.board[move.endRow][move.endCol] = move.pieceCaptured
-            self.whiteToMove = not self.whiteToMove
